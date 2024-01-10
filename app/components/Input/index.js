e0f314bc5d42880ad5195c75b5c9d23c6a815aac
Roman Jean-Elie <romanjeanelie@yahoo.fr>
2024-01-10 14:46:49 +0100

    Input > fix placeHolder null

--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -128,7 +128,7 @@ export default class Input {
   }
 
   // Write
-  toWrite({ delay = 0, animButtons = true, animLogos = true, type = null, placeholder = null, focus = true } = {}) {
+  toWrite({ delay = 0, animButtons = true, animLogos = true, type = null, placeholder = "", focus = true } = {}) {
     if (type === "imageQuestions") {
       if (this.currentStatus !== STATUS.UPLOAD_IMAGE) {
         this.anims.toWrite({ delay, animButtons, animLogos, placeholder, focus });
