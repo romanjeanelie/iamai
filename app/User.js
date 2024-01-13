c6026e72a811b1d445c16507db6fddfcef751be2
ponappu <ponappu@gmail.com>
2024-01-13 20:39:16 +0800

    login changes
    added one user class

--- app/User.js
+++ app/User.js
@@ -7,6 +7,80 @@ class User {
     this.name = name;
     this.picture = picture;
     this.email = email;
+    this.timezone = this.getUserTimezone();
+    this.area_name = "";
+    this.country_name = "";
+    this.pincode = "";
+    this.iso_2d_country_code = "";
+    this.administrative_area_level_1 = "";
+    this.administrative_area_level_2 = "";
+  }
+  async setuseraddress() {
+    let address = await this.getaddressdetails()
+    this.area_name = address.area_name;
+    this.country_name = address.country_name;
+    this.pincode = address.pincode;
+    this.iso_2d_country_code = address.iso_2d_country_code;
+    this.administrative_area_level_1 = address.administrative_area_level_1;
+    this.administrative_area_level_2 = address.administrative_area_level_2;
+  }
+
+  getUserTimezone() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  }
+  
+  getaddressdetails() {
+    return new Promise(async (resolve, reject) => {
+      let location = await this.getUserLocation();
+      location = JSON.parse(location);
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+          resolve(JSON.parse(this.responseText))
+        }
+      });
+      let url = "https://ai.iamplus.services/location/getaddress?latitude=" + location.lat + "&longitude=" + location.long;
+      console.log("URL:" + url)
+      xhr.open("GET", url);
+      xhr.send();
+    });
+  }
+  
+  getUserLocation() {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        console.log("Geolocation is not supported by your browser");
+        resolve(this.getipadress());
+        // reject(new Error("Geolocation is not supported by your browser"));
+      } else {
+        // navigator.geolocation.getCurrentPosition(success, getipadress);
+        navigator.geolocation.getCurrentPosition((pos) => {
+          console.log("got geo location");
+          resolve(JSON.stringify({ "lat": pos.coords.latitude, "long": pos.coords.longitude }))
+        },
+          (error) => {
+            console.log("Geolocation permission denied");
+            resolve(this.getipadress());
+          });
+      }
+    });
+  }
+  
+  getipadress = () => {
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest();
+      request.open('GET', 'https://api.ipdata.co/?api-key=edadfa1ba2f38b1066342735ae303338478afada8e5eeb770929fafd');
+      request.setRequestHeader('Accept', 'application/json');
+      request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          var data = JSON.parse(this.responseText)
+          console.log(`ip lat: ${data.latitude} long: ${data.longitude}`);
+          resolve(JSON.stringify({ "lat": data.latitude, "long": data.longitude }))
+        }
+      };
+      request.send();
+    });
   }
 }
 
@@ -24,10 +98,11 @@ function getCurrentUser(auth) {
 
 async function getUser() {
   return new Promise((resolve, reject) => {
-  getCurrentUser(auth).then(user => {
+  getCurrentUser(auth).then(async user => {
     if (user) {
       // User is signed in
       const loggedinuser = new User(user.uid, user.displayName, user.photoURL, user.email);
+      await loggedinuser.setuseraddress();
       console.log(" here logged in:", loggedinuser);
       resolve(loggedinuser);
     } else {
@@ -43,4 +118,5 @@ function redirectToLogin() {
   window.location.href = "./login/index.html";
 }
 
+export default User
 export { getUser, redirectToLogin };
--- login/index.js
+++ login/index.js
@@ -2,6 +2,7 @@ let latitude;
 let longitude;
 
 import auth from '../app/firebaseConfig';
+import User from '../app/User';
 import {
   GoogleAuthProvider,
   connectAuthEmulator,
@@ -60,52 +61,13 @@ onAuthStateChanged(auth, async function (user) {
   }
 });
 
-class User {
-  constructor(uuid, name, picture, email) {
-    this.uuid = uuid;
-    this.name = name;
-    this.picture = picture;
-    this.email = email;
-    this.timezone = getUserTimezone();
-    this.area_name = "";
-    this.country_name = "";
-    this.pincode = "";
-    this.iso_2d_country_code = "";
-    this.administrative_area_level_1 = "";
-    this.administrative_area_level_2 = "";
-  }
-  async setuseraddress() {
-    let address = await getaddressdetails()
-    this.area_name = address.area_name;
-    this.country_name = address.country_name;
-    this.pincode = address.pincode;
-    this.iso_2d_country_code = address.iso_2d_country_code;
-    this.administrative_area_level_1 = address.administrative_area_level_1;
-    this.administrative_area_level_2 = address.administrative_area_level_2;
-  }
-}
+
 
 async function redirectToHome(user) {
   console.log("redirect to home");
   let data = await getsessionID(user);
   window.location.href = "../index.html?lang=ad&session_id=" + data.SessionID + "&deploy_id=" + data.deploy_id;
 }
-
-// window.handleCredentialResponse = async (response) => {
-//   console.log(response);
-//   const responsePayload = decodeJwtResponse(response.credential);
-//   console.log(responsePayload);
-//   const user = new User(responsePayload.sub, responsePayload.name, responsePayload.picture, responsePayload.email);
-//   await user.setuseraddress();
-//   console.log(user);
-//   divgoogle.style.display = "none";
-//   localStorage.setItem("googleToken", response.credential);
-//   redirectToHome(user);
-
-// };
-
-
-
 
 const getsessionID = (user) => new Promise(function (resolve, reject) {
   // WARNING: For GET requests, body is set to null by browsers.
@@ -122,66 +84,8 @@ const getsessionID = (user) => new Promise(function (resolve, reject) {
   xhr.send(JSON.stringify(user));
 });
 
-function getUserTimezone() {
-  return Intl.DateTimeFormat().resolvedOptions().timeZone;
-}
 
-function getaddressdetails() {
-  return new Promise(async (resolve, reject) => {
-    let location = await getUserLocation();
-    location = JSON.parse(location);
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === 4) {
-        console.log(this.responseText);
-        resolve(JSON.parse(this.responseText))
-      }
-    });
-    let url = "https://ai.iamplus.services/location/getaddress?latitude=" + location.lat + "&longitude=" + location.long;
-    console.log("URL:" + url)
-    xhr.open("GET", url);
-    xhr.send();
-  });
-}
-
-function getUserLocation() {
-  return new Promise((resolve, reject) => {
-    if (!navigator.geolocation) {
-      console.log("Geolocation is not supported by your browser");
-      resolve(getipadress());
-      // reject(new Error("Geolocation is not supported by your browser"));
-    } else {
-      // navigator.geolocation.getCurrentPosition(success, getipadress);
-      navigator.geolocation.getCurrentPosition((pos) => {
-        console.log("got geo location");
-        resolve(JSON.stringify({ "lat": pos.coords.latitude, "long": pos.coords.longitude }))
-      },
-        (error) => {
-          console.log("Geolocation permission denied");
-          resolve(getipadress());
-        });
-    }
-  });
-}
-
-const getipadress = () => {
-  return new Promise((resolve, reject) => {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.ipdata.co/?api-key=edadfa1ba2f38b1066342735ae303338478afada8e5eeb770929fafd');
-    request.setRequestHeader('Accept', 'application/json');
-    request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        var data = JSON.parse(this.responseText)
-        console.log(`ip lat: ${data.latitude} long: ${data.longitude}`);
-        resolve(JSON.stringify({ "lat": data.latitude, "long": data.longitude }))
-      }
-    };
-    request.send();
-  });
-}
 window.onload = async function () {
   let signInButton = document.getElementById('divgoogle');
   signInButton.addEventListener('click', toggleSignIn, false);
 };
-
-export { auth }
