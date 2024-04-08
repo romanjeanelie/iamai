ad359ced713c86f39fa379e36869f545e250c1f2
Serotoninene <pujol.alexandre@hotmail.fr>
2024-04-08 18:50:47 +0200

    pushed the pin of the navbar below the hero video

---
+++ app/Blog/BlogData.js
@@ -0,0 +1,119 @@
+export const heroData = [
+  "TLDR",
+  "Introducing CO* <br /> World’s First <br /> Personal AI Assistant.",
+  "Revolutionise how you <br /> get things done.",
+  "CO* converses <br class='mobile-break' /> naturally and <br class='desktop-break' /> tackles real-world tasks like a <br class='desktop-break' /> human assistant.",
+  "Get instant answers. <br /> Not search results. ",
+  "Experience true multitasking.",
+  "CO* effortlessly <br class='sm-mobile-break' /> juggles multiple <br class='desktop-break' /> conversations and <br class='sm-mobile-break' /> tasks <br class='desktop-break' /> maximising <br class='sm-mobile-break' />  your time <br class='desktop-break' /> and efficiency.",
+  "CO* is multilingual.<br/> Speaks several languages fluently.",
+  "Powered by open-source <br class='desktop-break' /> innovation.",
+  "Our expert fine-tuning <br class='sm-mobile-break' /> of LLaMA <br class='desktop-break' /> 70B  <br class='sm-mobile-break' /> model has <br /> created a powerful <br /> Personal AI Assistant.",
+  "With <br /> Advanced planning. <br/> Advanced reasoning. <br />Task execution.",
+  "CO* tackles <br class='sm-mobile-break' />  complex tasks. <br />  Breaks tasks down.<br /> Execute steps. <br /> Adapts on the fly <br class='sm-mobile-break' />  for <br class='desktop-break' /> successful completion.",
+  "We all want more <br class='sm-mobile-break' />  time, less hassle.<br /> That's why we <br class='sm-mobile-break' />  created CO*",
+  "Personal <br class='sm-mobile-break' />  AI Assistant <br class='sm-mobile-break' />  For Everyone.",
+];
+
+export const slider1Data = [
+  {
+    id: 1,
+    video:
+      "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712325504/Desktop_Zoom_In_ChatGPT_q9c27m.mp4",
+    videoMobile:
+      "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712326125/Mobile_Zoom_In_ChatGPT_msk62g.mp4",
+    description: "ChatGPT in action.",
+  },
+  {
+    id: 2,
+    image: "/images/blog/slider1-2.png",
+    description: "The actual showtimes for Dune 2 at AMC Century City on Saturday March 9th.",
+    mobileFormat: true,
+  },
+  {
+    id: 3,
+    image: "/images/blog/slider1-3.png",
+    description: "The actual showtimes for Dune 2 at AMC Century City on Saturday March 9th.",
+    mobileFormat: true,
+  },
+];
+
+export const slider2Data = [
+  {
+    id: 1,
+    video:
+      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
+  },
+  {
+    id: 2,
+    video:
+      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    mobileFormat: true,
+  },
+  {
+    id: 3,
+    video:
+      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
+    description:
+      "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  },
+];
+
+export const data = [
+  {
+    h1: "Talk. Not Search.",
+    h4: "CO*",
+    h4Span: "/’co-star:/",
+    p: "Makes information effortless with conversation. Get insightful answers, quick updates and news, all while CO* assists with your daily tasks – your trusted assistant.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "Effortlessly <br class='mobile-break'/> Multilingual.",
+    p: "Jump from English to French to German, and back again – even toss in some Mandarin or Hindi – CO* effortlessly keeps up within a single, flowing conversation.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "CO* can see, <br class='mobile-break'/> speak and hear.",
+    p: "CO* goes beyond text. It sees (images), hears (your voice), and speaks (respond naturally), providing a truly personalized and intuitive experience.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "CO* gets things done.<br/> Like a real assistant.",
+    p: "Everyday Efficiency. CO*  takes the hassle out of your life and functions like a proactive assistant who coordinates seamlessly on your behalf.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "Travel Whiz.",
+    p: "CO* transforms the hassle of trip planning into a breeze, acting like a dedicated travel agent who understands your needs.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "Entertainment. <br class='mobile-break'/> Your Way.",
+    p: "CO* learns your tastes to deliver movie suggestions, reviews, and showtimes that fit your schedule. It can even book tickets for you.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "<span class='first-line'> Powerhouse for Planning.<br/> </span> Research and  <br class='mobile-break'/> Execution. ",
+    p: "It gathers information, synthesizes insights, and provides actionable summaries for efficient decision-making.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "E-commerce Reinvented.",
+    p: "Ultimate Price Sleuth. Forget scrolling through endless online stores. CO* is your tireless shopping assistant",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "Culinary Companion.",
+    p: "CO* transforms your kitchen experience. It scans your fridge, offers recipe suggestions based on what you have identifies missing ingredients and suggests the best places to buy them.",
+    sliderData: slider2Data,
+  },
+  {
+    h1: "The Multitasking Assistant.<br class='desktop-break'/> Always <br class='mobile-break'/> by Your Side.",
+    p: "CO * is your tireless multitasker. It juggles tasks simultaneously, ensuring smooth and efficient completion.",
+    sliderData: slider2Data,
+  },
+];
--- app/Blog/index.js
+++ app/Blog/index.js
@@ -3,8 +3,8 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import BlogSlider from "../components/BlogSlider";
 
 import { generateSliderHeader } from "../utils/generateSlider";
-import { preloadVideos } from "../utils/preloadVideos";
 
+import { heroData, slider1Data, data } from "./BlogData";
 import {
   blackBlockAnimation,
   cascadingFadeInText,
@@ -20,141 +20,13 @@ const isMobile = window.innerWidth < 640;
 // TO DO
 // [X] block the video when scroll past it
 // [X] add the unmute/mute button to the video
+// [] push the tryCoStart Button after the clock video
+// [] hide the tryCoStar button when on highlight video + any of the sliders
+// [] add a mute / unmute btn to the videos
+// [] link it to them all
 // [] compress the too large size videos
 // [] upload them to cloudinary
 // [] manage all the videos in the blog page
-
-const heroData = [
-  "TLDR",
-  "Introducing CO* <br /> World’s First <br /> Personal AI Assistant.",
-  "Revolutionise how you <br /> get things done.",
-  "CO* converses <br class='mobile-break' /> naturally and <br class='desktop-break' /> tackles real-world tasks like a <br class='desktop-break' /> human assistant.",
-  "Get instant answers. <br /> Not search results. ",
-  "Experience true multitasking.",
-  "CO* effortlessly <br class='sm-mobile-break' /> juggles multiple <br class='desktop-break' /> conversations and <br class='sm-mobile-break' /> tasks <br class='desktop-break' /> maximising <br class='sm-mobile-break' />  your time <br class='desktop-break' /> and efficiency.",
-  "CO* is multilingual.<br/> Speaks several languages fluently.",
-  "Powered by open-source <br class='desktop-break' /> innovation.",
-  "Our expert fine-tuning <br class='sm-mobile-break' /> of LLaMA <br class='desktop-break' /> 70B  <br class='sm-mobile-break' /> model has <br /> created a powerful <br /> Personal AI Assistant.",
-  "With <br /> Advanced planning. <br/> Advanced reasoning. <br />Task execution.",
-  "CO* tackles <br class='sm-mobile-break' />  complex tasks. <br />  Breaks tasks down.<br /> Execute steps. <br /> Adapts on the fly <br class='sm-mobile-break' />  for <br class='desktop-break' /> successful completion.",
-  "We all want more <br class='sm-mobile-break' />  time, less hassle.<br /> That's why we <br class='sm-mobile-break' />  created CO*",
-  "Personal <br class='sm-mobile-break' />  AI Assistant <br class='sm-mobile-break' />  For Everyone.",
-];
-
-const slider1Data = [
-  {
-    id: 1,
-    video:
-      "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712325504/Desktop_Zoom_In_ChatGPT_q9c27m.mp4",
-    videoMobile:
-      "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712326125/Mobile_Zoom_In_ChatGPT_msk62g.mp4",
-    description: "ChatGPT in action.",
-  },
-  {
-    id: 2,
-    image: "/images/blog/slider1-2.png",
-    description: "The actual showtimes for Dune 2 at AMC Century City on Saturday March 9th.",
-    mobileFormat: true,
-  },
-  {
-    id: 3,
-    image: "/images/blog/slider1-3.png",
-    description: "The actual showtimes for Dune 2 at AMC Century City on Saturday March 9th.",
-    mobileFormat: true,
-  },
-];
-
-const slider2Data = [
-  {
-    id: 1,
-    video:
-      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
-  },
-  {
-    id: 2,
-    video:
-      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    mobileFormat: true,
-  },
-  {
-    id: 3,
-    video:
-      "https://dl.dropbox.com/s/gftbpl8lczyg9dvoirjjs/kreyda_alpha_0911-hevc-safari.mp4?rlkey=8v48lqnlbtbe3neg86p02c4p5&dl=0",
-    description:
-      "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-  },
-];
-
-const data = [
-  {
-    h1: "Talk. Not Search.",
-    h4: "CO*",
-    h4Span: "/’co-star:/",
-    p: "Makes information effortless with conversation. Get insightful answers, quick updates and news, all while CO* assists with your daily tasks – your trusted assistant.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "Effortlessly <br class='mobile-break'/> Multilingual.",
-    p: "Jump from English to French to German, and back again – even toss in some Mandarin or Hindi – CO* effortlessly keeps up within a single, flowing conversation.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "CO* can see, <br class='mobile-break'/> speak and hear.",
-    p: "CO* goes beyond text. It sees (images), hears (your voice), and speaks (respond naturally), providing a truly personalized and intuitive experience.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "CO* gets things done.<br/> Like a real assistant.",
-    p: "Everyday Efficiency. CO*  takes the hassle out of your life and functions like a proactive assistant who coordinates seamlessly on your behalf.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "Travel Whiz.",
-    p: "CO* transforms the hassle of trip planning into a breeze, acting like a dedicated travel agent who understands your needs.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "Entertainment. <br class='mobile-break'/> Your Way.",
-    p: "CO* learns your tastes to deliver movie suggestions, reviews, and showtimes that fit your schedule. It can even book tickets for you.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "<span class='first-line'> Powerhouse for Planning.<br/> </span> Research and  <br class='mobile-break'/> Execution. ",
-    p: "It gathers information, synthesizes insights, and provides actionable summaries for efficient decision-making.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "E-commerce Reinvented.",
-    p: "Ultimate Price Sleuth. Forget scrolling through endless online stores. CO* is your tireless shopping assistant",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "Culinary Companion.",
-    p: "CO* transforms your kitchen experience. It scans your fridge, offers recipe suggestions based on what you have identifies missing ingredients and suggests the best places to buy them.",
-    sliderData: slider2Data,
-  },
-  {
-    h1: "The Multitasking Assistant.<br class='desktop-break'/> Always <br class='mobile-break'/> by Your Side.",
-    p: "CO * is your tireless multitasker. It juggles tasks simultaneously, ensuring smooth and efficient completion.",
-    sliderData: slider2Data,
-  },
-];
-
-const videoData = [
-  isMobile
-    ? "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712314254/blog/COstarSeq02Mobile01_tbpni9.mp4"
-    : "https://res.cloudinary.com/dfdqiqn98/video/upload/q_auto:best/v1712314224/blog/COstarSeq02_02_zeb2bd.mp4",
-  isMobile
-    ? "https://player.vimeo.com/progressive_redirect/playback/924982717/rendition/480p/file.mp4?loc=external&signature=8619fc24410a36d101d0290fb232f644ae087db9bb47dd7f6673e85b92b4a633"
-    : "https://player.vimeo.com/progressive_redirect/playback/924982649/rendition/360p/file.mp4?loc=external&signature=238d6fa86e19d03bb6b6a4ac9b4c92c64eb1ee769062b1b0d4435370d645e9f3",
-  isMobile
-    ? "https://player.vimeo.com/progressive_redirect/playback/924982682/rendition/480p/file.mp4?loc=external&signature=1aef2b4018851aa08562e87b014b6abd6fbdbebdef849a05edcf59f1b64db3f5"
-    : "https://player.vimeo.com/progressive_redirect/playback/924982615/rendition/360p/file.mp4?loc=external&signature=ab8215600529b576ca8efa77be6acb4aa0ea78dcc8d2b11b17bfa0961caa68ca",
-];
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -196,8 +68,6 @@ class Blog {
   initHeroSections() {
     heroData.forEach((data, idx) => {
       const firstItem = idx === 0;
-      const isMobile = window.innerWidth < 640;
-
       // create the hero section
       const container = document.createElement("div");
       container.className = `blogHero__section ${firstItem && "first-section"}`;
@@ -409,10 +279,6 @@ class Blog {
     window.addEventListener("resize", () => {
       this.isMobile = window.innerWidth < 820;
     });
-  }
-
-  preloadStaticVideos() {
-    preloadVideos(videoData, { onComplete: () => console.log("All videos preloaded") });
   }
 }
 
