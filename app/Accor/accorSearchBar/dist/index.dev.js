290bd1322dcb7e5dcabcff19c469c83d7dda49ab
Serotoninene <pujol.alexandre@hotmail.fr>
2024-04-11 19:20:30 +0200

    made transitions between minimized, textinput and standard options

--- app/Accor/accorSearchBar/dist/index.dev.js
+++ app/Accor/accorSearchBar/dist/index.dev.js
@@ -1,15 +1,29 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
-var _gsap = _interopRequireDefault(require("gsap"));
+var _gsap = _interopRequireWildcard(require("gsap"));
 
 var _Flip = _interopRequireDefault(require("gsap/Flip"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -40,7 +54,7 @@ function () {
     this.searchBar = document.querySelector(".accorSearchBar__container");
     this.writeBtn = document.querySelector(".accorSearchBar__write-btn");
     this.expandBtn = document.querySelector(".accorSearchBar__expand-btn");
-    this.callBtn = document.querySelector(".accorSearchBar__phone-btn");
+    this.callBtn = document.querySelector(".accorSearchBar__action-btn");
     this.advancedBtn = document.querySelector(".accorSearchBar__advanced-btn"); // Init
 
     this.addEventListener();
@@ -49,9 +63,33 @@ function () {
   _createClass(AccorSearchBar, [{
     key: "switchStateClass",
     value: function switchStateClass(state) {
-      console.log();
-      this.searchBarState = state;
-      this.wrapper.className = "accorNavbar__item accorSearchBar__wrapper ".concat(state);
+      var _this$wrapper$classLi;
+
+      this.searchBarState = state; // grab state
+
+      var initialState = _Flip["default"].getState([this.searchBar, this.advancedBtn]);
+
+      (_this$wrapper$classLi = this.wrapper.classList).remove.apply(_this$wrapper$classLi, _toConsumableArray(Object.values(STATES)));
+
+      this.wrapper.classList.add(state);
+
+      _gsap["default"].killTweensOf(".standard-input");
+
+      _gsap["default"].set(".standard-input", {
+        opacity: 0
+      }); // Animate from the initial state to the end state
+
+
+      _Flip["default"].from(initialState, {
+        duration: 0.5,
+        ease: "power3.out",
+        absolute: true,
+        onComplete: function onComplete() {
+          _gsap["default"].to(".standard-input", {
+            opacity: 1
+          });
+        }
+      });
     }
   }, {
     key: "toMinimized",
--- app/Accor/accorSearchBar/index.js
+++ app/Accor/accorSearchBar/index.js
@@ -1,4 +1,4 @@
-import gsap from "gsap";
+import gsap, { Power2 } from "gsap";
 import Flip from "gsap/Flip";
 
 const STATES = {
@@ -21,7 +21,7 @@ export default class AccorSearchBar {
     this.searchBar = document.querySelector(".accorSearchBar__container");
     this.writeBtn = document.querySelector(".accorSearchBar__write-btn");
     this.expandBtn = document.querySelector(".accorSearchBar__expand-btn");
-    this.callBtn = document.querySelector(".accorSearchBar__phone-btn");
+    this.callBtn = document.querySelector(".accorSearchBar__action-btn");
     this.advancedBtn = document.querySelector(".accorSearchBar__advanced-btn");
 
     // Init
@@ -29,9 +29,27 @@ export default class AccorSearchBar {
   }
 
   switchStateClass(state) {
-    console.log();
     this.searchBarState = state;
-    this.wrapper.className = `accorNavbar__item accorSearchBar__wrapper ${state}`;
+
+    // grab state
+    const initialState = Flip.getState([this.searchBar, this.advancedBtn]);
+    this.wrapper.classList.remove(...Object.values(STATES));
+    this.wrapper.classList.add(state);
+
+    gsap.killTweensOf(".standard-input");
+    gsap.set(".standard-input", { opacity: 0 });
+
+    // Animate from the initial state to the end state
+    Flip.from(initialState, {
+      duration: 0.5,
+      ease: "power3.out",
+      absolute: true,
+      onComplete: () => {
+        gsap.to(".standard-input", {
+          opacity: 1,
+        });
+      },
+    });
   }
 
   toMinimized() {
@@ -55,6 +73,7 @@ export default class AccorSearchBar {
         this.toMinimized();
       }
     });
+
     document.addEventListener("click", (event) => {
       if (!this.searchBar.contains(event.target)) {
         this.toMinimized();
