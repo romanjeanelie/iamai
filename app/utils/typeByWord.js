86ba1ac22ffeef70d9843c1f8e781afb2a517948
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-02 12:24:11 +0200

    improved the getRemarkable function by appending the answerSpan at the start of the function

--- app/utils/typeByWord.js
+++ app/utils/typeByWord.js
@@ -2,43 +2,23 @@ import getRemarkable from "./getRemarkable";
 
 export default function typeByWord(container, text) {
   let wordIndex = 0;
-  let isStrong = false;
+
+  let answerSpan = document.createElement("span");
+  answerSpan.className = "AIanswer";
+  container.appendChild(answerSpan);
 
   const md = getRemarkable();
 
   return new Promise((resolve) => {
-    
-
     let words = text.split(" ");
 
     function type() {
-      let wordSpan;
-      if (container.querySelector(".AIword")) {
-        wordSpan = container.querySelector(".AIword")
-      }else{
-        wordSpan = document.createElement("span");
-        wordSpan.className = "AIword";
-        container.appendChild(wordSpan);
-      }
-
       if (wordIndex < words.length) {
+        let content;
 
-
-        // Check if the word is bold
-        // if (words[wordIndex].includes("<strong>")) isStrong = true;
-        // if (words[wordIndex].includes("</strong>")) {
-        //   wordSpan.classList.add("bold");
-        //   isStrong = false;
-        // }
-
-        // if (isStrong) wordSpan.classList.add("bold");
-        
-        let content = wordSpan.innerHTML+ (words[wordIndex]);
+        content = answerSpan.innerHTML + words[wordIndex];
         const markdownOutput = md.renderInline(content);
-        wordSpan.innerHTML = markdownOutput + " " ;
-        // wordSpan.innerHTML = wordSpan.innerHTML+(words[wordIndex] + " ");
-
-        //  Append the span to the container
+        answerSpan.innerHTML = markdownOutput + " ";
 
         wordIndex++; // Move to the next word
         setTimeout(type, 10); // Call this function again after a delay to simulate typing speed
