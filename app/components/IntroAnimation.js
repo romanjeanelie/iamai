9cc3078833def37569afd405cf5211c6d8cba42a
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-09 10:56:06 +0200

    made the introAnim responsive

--- app/components/IntroAnimation.js
+++ app/components/IntroAnimation.js
@@ -12,29 +12,23 @@ export class IntroAnimation {
     animateString(0, ["Hello."], this.introText, "", () => {
       this.introText.style.display = "none";
       this.introLogo.style.display = "block";
-
       const logoP = this.introLogo.querySelector("p");
       const logo = this.introLogo.querySelector(".co-logo");
-
       gsap.set([logoP, logo], {
         opacity: 0,
       });
-
       const tl = gsap.timeline({
         defaults: {
           ease: Power3.easeOut,
         },
       });
-
       tl.to(logoP, {
         opacity: 0.6,
       });
-
       tl.to(logo, {
         opacity: 1,
         duration: 1,
       });
-
       tl.to(this.introContainer, {
         yPercent: -100,
         duration: 0.7,
