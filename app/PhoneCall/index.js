db3382eccd2b9bc3d55169b285c4f2e804b3f93b
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-17 18:30:11 +0200

    set up the scroll indicator logic

--- app/PhoneCall/index.js
+++ app/PhoneCall/index.js
@@ -3,6 +3,31 @@ import { createNanoEvents } from "nanoevents";
 class PhoneCallPage {
   constructor() {
     this.emitter = createNanoEvents();
+    // States
+
+    // DOM Elements
+    this.herobanners = document.querySelector(".herobanners__container");
+    this.darkSection = document.querySelector(".herobanner__container.dark-section");
+    this.lightSection = document.querySelector(".herobanner__container.light-section");
+    this.scrollIndicator = document.querySelector(".herobanner__scroll-indicator-container");
+
+    // Methods
+    this.handleSwitchSections();
+  }
+
+  handleSwitchSections() {
+    this.herobanners.addEventListener("scroll", (e) => {
+      const maxScrollLeft = e.target.scrollWidth - e.target.clientWidth;
+      const scrollValueNormalized = e.target.scrollLeft / maxScrollLeft;
+
+      if (scrollValueNormalized > 0.5) {
+        this.scrollIndicator.classList.add("light-section");
+        this.scrollIndicator.classList.remove("dark-section");
+      } else {
+        this.scrollIndicator.classList.add("dark-section");
+        this.scrollIndicator.classList.remove("light-section");
+      }
+    });
   }
 }
 
