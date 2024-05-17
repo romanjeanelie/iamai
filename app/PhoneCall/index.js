68ab2dbeff0861d39aaa00419c45cc0f16ce5234
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-17 18:44:12 +0200

    animated the scroll indicators

--- app/PhoneCall/index.js
+++ app/PhoneCall/index.js
@@ -1,9 +1,14 @@
 import { createNanoEvents } from "nanoevents";
+import gsap from "gsap";
+import { Flip } from "gsap/Flip";
+
+gsap.registerPlugin(Flip);
 
 class PhoneCallPage {
   constructor() {
     this.emitter = createNanoEvents();
     // States
+    this.section = "dark";
 
     // DOM Elements
     this.herobanners = document.querySelector(".herobanners__container");
@@ -15,17 +20,30 @@ class PhoneCallPage {
     this.handleSwitchSections();
   }
 
+  animateScrollIndicators() {
+    const indicators = this.scrollIndicator.querySelectorAll(".herobanner__scroll-indicator");
+    const initialState = Flip.getState([indicators]);
+    this.scrollIndicator.classList.remove("dark", "light");
+    this.scrollIndicator.classList.add(this.section);
+    Flip.from(initialState, {
+      duration: 0.4,
+      ease: "power3.out",
+    });
+  }
+
   handleSwitchSections() {
     this.herobanners.addEventListener("scroll", (e) => {
       const maxScrollLeft = e.target.scrollWidth - e.target.clientWidth;
       const scrollValueNormalized = e.target.scrollLeft / maxScrollLeft;
 
       if (scrollValueNormalized > 0.5) {
-        this.scrollIndicator.classList.add("light-section");
-        this.scrollIndicator.classList.remove("dark-section");
+        if (this.section === "light") return;
+        this.section = "light";
+        this.animateScrollIndicators();
       } else {
-        this.scrollIndicator.classList.add("dark-section");
-        this.scrollIndicator.classList.remove("light-section");
+        if (this.section === "dark") return;
+        this.section = "dark";
+        this.animateScrollIndicators();
       }
     });
   }
