f24cc798340163b35cf7ef69fd2ce5536d8d6770
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-06-12 02:12:00 +0200

    set the maxPhotos as 5

--- app/components/Input/InputVideo.js
+++ app/components/Input/InputVideo.js
@@ -45,7 +45,7 @@ export default class InputVideo {
     // photos states
     this.captureInterval = null;
     this.photos = [];
-    this.maxPhotos = 2;
+    this.maxPhotos = 5;
     this.currentPhotoIndex = 0;
 
     // Dom elements
