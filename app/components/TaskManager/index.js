73c0450efd5200a4c7ecd56efa13cd1dbe855132
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-08-26 10:30:24 +0200

    added back the logic for taskManager button

--- app/components/TaskManager/index.js
+++ app/components/TaskManager/index.js
@@ -43,7 +43,7 @@ export default class TaskManager {
     // DOM Elements
     this.html = document.documentElement;
     this.container = document.querySelector(".task-manager__container");
-    this.button = document.querySelector(".footer-nav__tasks-container");
+    this.navButton = document.querySelector(".footer-nav__tasks-container");
     this.closeButton = document.querySelector(".task-manager__closing-icon");
     this.discussionContainer = document.querySelector(".discussion__wrapper");
     this.fullscreenButton = document.querySelector(".task-manager__fullscreen-icon");
@@ -166,6 +166,48 @@ export default class TaskManager {
       },
       "deleteTask"
     );
+  }
+
+  // ---------- Handling the task-manager button ----------
+  getButtonColor() {
+    // order of priority for the color of the button
+    const order = [TASK_STATUSES.COMPLETED, TASK_STATUSES.INPUT_REQUIRED, TASK_STATUSES.IN_PROGRESS];
+    for (const status of order) {
+      // the first status found in the tasks array will be the color of the button
+      if (this.tasks.some((task) => task.status.type === status)) {
+        if (status === TASK_STATUSES.COMPLETED) {
+          this.button.classList.add("completed");
+        } else {
+          this.button.classList.remove("completed");
+        }
+
+        return STATUS_COLORS[status];
+      }
+    }
+  }
+
+  initializeButton() {
+    this.button.classList.remove("hidden");
+    this.updateButton();
+  }
+
+  updateButton() {
+    this.button.innerHTML = this.tasks.length;
+    this.button.style.backgroundColor = this.getButtonColor();
+  }
+
+  removeButton() {
+    this.button.classList.add("hidden");
+  }
+
+  handleButton() {
+    if (this.tasks.length === 0) {
+      this.removeButton();
+    } else if (this.tasks.length === 1) {
+      this.initializeButton();
+    } else {
+      this.updateButton();
+    }
   }
 
   // ---------- Handling the task-manager states ----------
@@ -587,7 +629,7 @@ export default class TaskManager {
   }
 
   addListeners() {
-    this.button.addEventListener("click", () => this.toFullscreen());
+    this.navButton.addEventListener("click", () => this.toFullscreen());
     this.closeButton.addEventListener("click", () => this.closeTaskManager());
     this.fullscreenButton.addEventListener("click", () => this.toFullscreen());
     this.closeFullscreenButton.addEventListener("click", () => this.closeTaskManager());
