22e3193c686a1221654af2dc9922e0dc944ae897
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-08-28 23:43:40 +0200

    adjusted the discussion font styles to the new design

--- app/components/Phone/index.js
+++ app/components/Phone/index.js
@@ -87,7 +87,7 @@ export default class Phone {
     this.debugFlights = false;
 
     if (this.debug) {
-      this.anims.toStartPhoneRecording();
+      // this.anims.toStartPhoneRecording();
       // this.startConnecting();
     }
   }
