445397c3a112e729f51bef5db53b4c3f012066ea
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-11-05 17:53:51 +0100

    HISTORY : refactored the History and HistoryFetcher so we only get the data from historyFetcher and make the UI in History + creating only one historyContainer and not two as it was originally

--- app/components/History/HistoryFetcher.js
+++ app/components/History/HistoryFetcher.js
@@ -1,11 +1,5 @@
 import fetcher from "../../utils/fetcher.js";
-import getMarked from "../../utils/getMarked.js";
-import DiscussionMedia from "../DiscussionMedia.js";
 import { URL_CONVERSATION_HISTORY } from "../constants.js";
-
-const isEmpty = (obj) => Object.keys(obj).length === 0;
-// const md = getRemarkable();
-const md = getMarked();
 
 export function getPreviousDayTimestamp() {
   const currentDate = new Date();
@@ -42,91 +36,18 @@ export default class HistoryFetcher {
     return data;
   }
 
-  createUIElements(elements) {
-    const container = document.createElement("div");
-
-    elements.forEach((element) => {
-      if (element.user.length > 0) {
-        const userContainer = document.createElement("div");
-        userContainer.classList.add("discussion__user");
-        var userContainerspan = document.createElement("span");
-        userContainerspan.classList.add("discussion__userspan");
-        // const userTextMarkdowned = md.renderInline(element.user);
-        userContainerspan.innerHTML = element.user;
-        userContainer.appendChild(userContainerspan);
-        container.appendChild(userContainer);
-
-        // 1st way to figure out if an img comes from the video input - the length
-        // const isImgsComingFromVideo = element.images.user_images?.length > 40000;
-
-        // 2nd way to figure out if an img comes from the video input - the presence of 'data:image/png;base64,'
-        const isImgsComingFromVideo = element.images.user_images?.includes("data:image/png;base64,");
-
-        if (!isEmpty(element.images) && !isImgsComingFromVideo) {
-          const media = new DiscussionMedia({
-            container: userContainer,
-            emitter: this.emitter,
-          });
-          if (element.images.user_images) media?.addUserImages(JSON.parse(element.images.user_images));
-          container.appendChild(userContainer);
-        }
-      }
-
-      if (element.assistant.length > 0) {
-        const AIContainer = document.createElement("div");
-        AIContainer.classList.add("discussion__ai");
-
-        // Need to stringify
-        const string = JSON.stringify(element.assistant);
-
-        // Remove leading and trailing quotes
-        const textWithoutQuotes = string.slice(1, -1);
-
-        // Replace \n with <br>
-        const assistantText = textWithoutQuotes.replace(/\\n/g, "<br>");
-
-        // Render the markdown
-        const assistantTextMardowned = md.parse(assistantText);
-
-        AIContainer.innerHTML = assistantTextMardowned;
-        container.appendChild(AIContainer);
-
-        if (!isEmpty(element.sources) || !isEmpty(element.images)) {
-          const media = new DiscussionMedia({
-            container: AIContainer,
-            emitter: this.emitter,
-          });
-          if (element.images.images) {
-            media.initImages();
-            media?.addImages(JSON.parse(element.images.images).slice(0, 8));
-          }
-          if (element.sources.sources) {
-            media?.addSources(JSON.parse(element.sources.sources));
-          }
-
-          container.appendChild(AIContainer);
-        }
-      }
-    });
-    return container;
-  }
-
   async getHistory({ uuid, user, size = 3 }) {
     this.isFetching = true;
     // Get elements
-    const elements = await this.getAllElements({ uuid, user, size, start: this.newStart });
+    const data = await this.getAllElements({ uuid, user, size, start: this.newStart });
     // Reverse the order of elements
 
-    elements.results.reverse();
-
-    // Create UI elements
-    const container = this.createUIElements(elements.results);
-    container.classList.add("history__container");
+    data.results.reverse();
 
     this.isSet = true;
     this.isFetching = false;
     this.newStart += size;
 
-    return { elements: elements.results, container };
+    return data.results;
   }
 }
--- app/components/History/index.js
+++ app/components/History/index.js
@@ -1,5 +1,9 @@
+import getMarked from "../../utils/getMarked";
 import { store } from "../store";
 import HistoryFetcher from "./HistoryFetcher";
+
+const md = getMarked();
+const isEmpty = (obj) => Object.keys(obj).length === 0;
 
 export default class History {
   constructor({ emitter }) {
@@ -13,6 +17,74 @@ export default class History {
     this.addListeners();
   }
 
+  createUIElements(data) {
+    console.log(data);
+    data.forEach((element) => {
+      if (element.user.length > 0) {
+        const userContainer = document.createElement("div");
+        userContainer.classList.add("discussion__user");
+        var userContainerspan = document.createElement("span");
+        userContainerspan.classList.add("discussion__userspan");
+        // const userTextMarkdowned = md.renderInline(element.user);
+        userContainerspan.innerHTML = element.user;
+        userContainer.appendChild(userContainerspan);
+        this.historyContainer.appendChild(userContainer);
+
+        // 1st way to figure out if an img comes from the video input - the length
+        // const isImgsComingFromVideo = element.images.user_images?.length > 40000;
+
+        // 2nd way to figure out if an img comes from the video input - the presence of 'data:image/png;base64,'
+        const isImgsComingFromVideo = element.images.user_images?.includes("data:image/png;base64,");
+
+        if (!isEmpty(element.images) && !isImgsComingFromVideo) {
+          const media = new DiscussionMedia({
+            container: userContainer,
+            emitter: this.emitter,
+          });
+          if (element.images.user_images) media?.addUserImages(JSON.parse(element.images.user_images));
+          container.appendChild(userContainer);
+        }
+      }
+
+      if (element.assistant.length > 0) {
+        const AIContainer = document.createElement("div");
+        AIContainer.classList.add("discussion__ai");
+
+        // Need to stringify
+        const string = JSON.stringify(element.assistant);
+
+        // Remove leading and trailing quotes
+        const textWithoutQuotes = string.slice(1, -1);
+
+        // Replace \n with <br>
+        const assistantText = textWithoutQuotes.replace(/\\n/g, "<br>");
+
+        // Render the markdown
+        const assistantTextMardowned = md.parse(assistantText);
+
+        AIContainer.innerHTML = assistantTextMardowned;
+        this.historyContainer.appendChild(AIContainer);
+
+        if (!isEmpty(element.sources) || !isEmpty(element.images)) {
+          const media = new DiscussionMedia({
+            container: AIContainer,
+            emitter: this.emitter,
+          });
+          if (element.images.images) {
+            media.initImages();
+            media?.addImages(JSON.parse(element.images.images).slice(0, 8));
+          }
+          if (element.sources.sources) {
+            media?.addSources(JSON.parse(element.sources.sources));
+          }
+
+          this.historyContainer.appendChild(AIContainer);
+        }
+      }
+    });
+    return this.historyContainer;
+  }
+
   async updateHistory() {
     // hide the previous discussion container while it is loading to avoid scroll jumps
     this.historyContainer.style.display = "none";
@@ -21,8 +93,10 @@ export default class History {
       const chatId = store.get("chatId");
       const user = store.get("user");
 
-      const { container } = await this.fetcher.getHistory({ uuid: chatId, user, size: 10 });
-      this.historyContainer.appendChild(container);
+      const data = await this.fetcher.getHistory({ uuid: chatId, user, size: 10 });
+
+      this.createUIElements(data);
+
       const imgs = this.historyContainer.querySelectorAll("img");
       let imgLoadedCount = 0;
       const totalImages = imgs.length;
